8efb408eeb3ac1c274ab0914ac06d12bd49386fa
Samuel <mbuthia.jsamuel@gmail.com>
2020-03-27 12:22:14 +0300

    Set up JS development environment

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,20 +1,14 @@
-const express = require('express');
-const path = require ('path');
-const open = require ('open'); //ref to open a site in the browser
+import express from 'express';
+import path from 'path';
+import open from'open'; //ref to open a site in the browser
 
 const port = 3000;//variable stores the port to be used
 const app = express(); //To create an instance of express
 
 // Tell express which route it should handle
-app.get('/', function(req, res){
+app.get('/',(req, res)=>{
   res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
 //Tell Express to listen on the port we defined above
-app.listen(port, function(err){
-  if (err){
-    console.log(err)
-  }else{
-    open('http://localhost:' + port);
-  }
-});
+app.listen(port,()=> {open('http://localhost:' + port);});
